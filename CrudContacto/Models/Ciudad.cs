83741a16a71d4b21246dd0339dda311663956358
Nelson Geovanny Ortiz Morales <nortiz@mobiletec.net>
2024-08-07 17:32:05 -0500

    Se agrega la clase ciudad

---
+++ CrudContacto/Models/Ciudad.cs
@@ -0,0 +1,9 @@
+﻿namespace CrudContacto.Models
+{
+    public class Ciudad
+    {
+        public string nombre { get; set; };
+        public string departamento { get; set; };
+        public string pais { get; set; };
+    }
+}
